52114fd0f62e95bfa7245ecf9136eb0a86cb0a23
Qingfeng Liu <maculosa@163.com>
2024-05-24 10:38:51 +0800

    chore: remove procomponent

--- packages/procomponent/components/ProTable/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import DensityComponent from './button/Density.vue'
-import RefreshComponent from './button/Refresh.vue'
-import CreateComponent from './button/Create.vue'
-
-export const DensityButton = DensityComponent
-export const RefreshButton = RefreshComponent
-export const CreateButton = CreateComponent
