42ba195c7b2ae1b500605ed6a51b1c1558764ca2
Qingfeng Liu <maculosa@163.com>
2024-07-06 09:16:09 +0800

    chore: 移除全局安装组件库

--- packages/procomponent/index.ts
+++ packages/procomponent/index.ts
@@ -3,27 +3,27 @@
  *
  * 根据 /packages 目录下的组件所生成的模块导出，请勿手动修改
  */
-import type { App, Plugin } from 'vue'
+// import type { App, Plugin } from 'vue'
 
-import { DrawerFormPlugin } from './components/DrawerForm'
-import { ModalFormPlugin } from './components/ModalForm'
-import { ProFormPlugin } from './components/ProForm'
-import { ProTablePlugin } from './components/ProTable'
-import { ProTextPlugin } from './components/ProText'
-import { QueryFilterPlugin } from './components/QueryFilter'
+// import { DrawerFormPlugin } from './components/DrawerForm'
+// import { ModalFormPlugin } from './components/ModalForm'
+// import { ProFormPlugin } from './components/ProForm'
+// import { ProTablePlugin } from './components/ProTable'
+// import { ProTextPlugin } from './components/ProText'
+// import { QueryFilterPlugin } from './components/QueryFilter'
 
-const BanmaoDemoPlugin: Plugin = {
-  install(app: App) {
-    DrawerFormPlugin.install?.(app)
-    ModalFormPlugin.install?.(app)
-    ProFormPlugin.install?.(app)
-    ProTablePlugin.install?.(app)
-    ProTextPlugin.install?.(app)
-    QueryFilterPlugin.install?.(app)
-  },
-}
+// const BanmaoDemoPlugin: Plugin = {
+//   install(app: App) {
+//     DrawerFormPlugin.install?.(app)
+//     ModalFormPlugin.install?.(app)
+//     ProFormPlugin.install?.(app)
+//     ProTablePlugin.install?.(app)
+//     ProTextPlugin.install?.(app)
+//     QueryFilterPlugin.install?.(app)
+//   },
+// }
 
-export default BanmaoDemoPlugin
+// export default BanmaoDemoPlugin
 
 export * from './components/DrawerForm'
 export * from './components/ModalForm'
