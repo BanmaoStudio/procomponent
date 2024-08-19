03c6b980f9e3bfefd2e00fb71094b563a5d4bf1f
Qingfeng Liu <maculosa@163.com>
2024-08-19 17:51:10 +0800

    feat(ProList): 添加 ProList

---
+++ packages/procomponent/components/ProList/src/index.tsx
@@ -0,0 +1,46 @@
+import { NFlex, NList, NListItem, NPagination } from 'naive-ui'
+import type { VNodeChild } from 'vue'
+
+export default defineComponent({
+  name: 'ProList',
+  props: {
+    pagination: {
+      type: Object,
+      default: () => ({
+        page: 1,
+        pageSize: 10,
+        total: 0,
+      })
+    },
+    title: String,
+    renderItem: {
+      type: Function as PropType<(row: any) => VNodeChild>,
+    },
+    data: {
+      type: Array as PropType<any[]>,
+      default: () => []
+    }
+  },
+  setup(props) {
+
+    return () => {
+      return (
+        <NFlex vertical>
+          <NList>
+            {{
+              header: '',
+              footer: '',
+              default: () =>
+                props.data.map((item) => {
+                  return <NListItem>{props.renderItem && props.renderItem(item)}</NListItem>
+                })
+            }}
+          </NList>
+          <div class="flex" style="justify-content: end;">
+            <NPagination {...props.pagination } />
+          </div>
+        </NFlex>
+      )
+    }
+  }
+})
