ed269b0727d232889d5153b1b963d91b49805c10
Qingfeng Liu <maculosa@163.com>
2024-08-19 17:52:03 +0800

    feat(ProDescriptions): 添加ProDescriptions

---
+++ docs/procomponent/pro-descriptions/list-index.vue
@@ -0,0 +1,41 @@
+<script setup lang="tsx">
+  import { ref } from 'vue'
+  import { ProDescriptions } from '@banmao/procomponent'
+
+  const dataSource = ref({
+    id: 1,
+    name: '张三',
+    age: 18,
+    gender: 'male',
+    address: 'asdasdasdasdasd'
+  })
+
+  const columns = ref([
+    { title: 'ID', key: 'id' },
+    { title: '姓名', key: 'name' },
+    {
+      title: '性别',
+      key: 'gender',
+      span: 2,
+      render(row) {
+        return row.gender === 'male' ? '男' : '女'
+      }
+    }
+  ])
+
+  const loading = ref(false)
+</script>
+
+<template>
+  <ProDescriptions
+    title="详情"
+    label-placement="left"
+    size="large"
+    :column="2"
+    :bordered="false"
+    :columns="columns"
+    :data="dataSource"
+    :loading="loading" />
+</template>
+
+<script setup lang="tsx"></script>
---
+++ packages/procomponent/components/ProDescriptions/src/index.tsx
@@ -0,0 +1,49 @@
+import { NDescriptions, NDescriptionsItem, descriptionsProps } from 'naive-ui'
+
+export default defineComponent({
+  name: 'ProDescriptions',
+  props: {
+    ...descriptionsProps,
+    title: {
+      type: String,
+      default: 'Description List'
+    },
+    data: {
+      type: Object,
+      default: () => ({})
+    },
+    columns: {
+      type: Array as PropType<{ [key: string]: any }[]>,
+      default: () => []
+    }
+  },
+  setup(props) {
+    const { title, data, columns, ...descriptionsProps } = props
+
+    return () => (
+      <NDescriptions title={title} {...descriptionsProps}>
+        {columns.map((item) => {
+          if (item && item.render) {
+            return (
+              <NDescriptionsItem
+                key={item.key}
+                label={item.title}
+                span={item.span}>
+                {item.render(data[item.key])}
+              </NDescriptionsItem>
+            )
+          } else {
+            return (
+              <NDescriptionsItem
+                key={item.key}
+                label={item.title}
+                span={item.span}>
+                {data[item.key]}
+              </NDescriptionsItem>
+            )
+          }
+        })}
+      </NDescriptions>
+    )
+  }
+})
