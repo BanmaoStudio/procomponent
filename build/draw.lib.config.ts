8d660730bece568a60ee5d699c5ae0047f9d0e52
Qingfeng Liu <maculosa@163.com>
2024-08-20 17:16:11 +0800

    feat(draw): 添加 draw-core

---
+++ build/draw.lib.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import libCss from 'vite-plugin-libcss'
+import baseConfig from './base.config'
+
+export default defineConfig({
+  ...baseConfig,
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, '../packages/draw/index.ts'),
+      name: '@banmao/procomponent',
+      fileName: format => `procomponent.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue', 'uno.css', 'naive-ui'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    ...(baseConfig as any).plugins,
+    dts(),
+    libCss(),
+  ],
+})
