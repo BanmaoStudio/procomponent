7ad7ea9a95e86bc5fd25f78eb2a02544d7639820
Qingfeng Liu <maculosa@163.com>
2024-08-21 16:14:19 +0800

    chore: remove example

--- packages/example/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import datav from '@banmao/datav-vue3'
-
-import './style.css'
-import App from './App.vue'
-import 'uno.css'
-
-createApp(App).use(datav).mount('#app')
