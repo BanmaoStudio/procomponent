daf15509ac324eb796acf9479dfb3570bcddb487
lqf <maculosa@163.com>
2024-10-17 15:19:56 +0800

    fix: compliant clipboardText

--- packages/procomponent/components/ProText/src/index.vue
+++ packages/procomponent/components/ProText/src/index.vue
@@ -1,76 +1,83 @@
 <template>
-  <UseClipboard v-slot="{ copy, copied }" legacy :source="text">
-    <NTooltip v-if="ellipsis" trigger="hover">
-      <template #trigger>
-        <TextClamp :max-lines="maxLines" :text="text">
-          <template #after>
-            <Icon
-              v-if="copyable"
-              :style="{
-                ...bmIconStyle,
-                color: copied ? copiedColor : defaultColor
-              }"
-              :icon="`ant-design:${copied ? 'check' : 'copy'}-outlined`"
-              @click="copy()" />
-          </template>
-        </TextClamp>
-      </template>
-      <div style="max-width: 300px">
-        <span>{{ text }}</span>
-      </div>
-    </NTooltip>
-    <template v-else>
-      <NText v-bind="$attrs">
-        {{ text }}
-      </NText>
-      <Icon
-        v-if="copyable"
-        :style="{
-          ...bmIconStyle,
-          color: copied ? copiedColor : defaultColor
-        }"
-        :icon="`ant-design:${copied ? 'check' : 'copy'}-outlined`"
-        @click="copy()" />
+  <NTooltip v-if="ellipsis" trigger="hover">
+    <template #trigger>
+      <TextClamp :max-lines="maxLines" :text="text">
+        <template #after>
+          <Icon v-if="copyable" :style="{
+            ...bmIconStyle,
+            color: copied ? copiedColor : defaultColor
+          }" :icon="`ant-design:${copied ? 'check' : 'copy'}-outlined`" @click="handleCopy" />
+        </template>
+      </TextClamp>
     </template>
-  </UseClipboard>
+    <div style="max-width: 300px">
+      <span>{{ text }}</span>
+    </div>
+  </NTooltip>
+  <template v-else>
+    <NText v-bind="$attrs">
+      {{ text }}
+    </NText>
+    <Icon v-if="copyable" :style="{
+      ...bmIconStyle,
+      color: copied ? copiedColor : defaultColor
+    }" :icon="`ant-design:${copied ? 'check' : 'copy'}-outlined`" @click="handleCopy" />
+  </template>
 </template>
 
 <script setup lang="ts">
-  import { UseClipboard } from '@vueuse/components'
-  import { Icon } from '@iconify/vue'
-  import { NText, NTooltip } from 'naive-ui'
-  import TextClamp from 'vue3-text-clamp'
+import { useClipboard } from '@vueuse/core';
+import { Icon } from '@iconify/vue'
+import { NText, NTooltip } from 'naive-ui'
+import TextClamp from 'vue3-text-clamp'
 
-  defineOptions({
-    name: 'ProText'
-  })
+defineOptions({
+  name: 'ProText'
+})
 
-  const props = withDefaults(defineProps<ProTextProps>(), {
-    copyable: false,
-    lineClamp: 1
-  })
+const props = withDefaults(defineProps<ProTextProps>(), {
+  copyable: false,
+  lineClamp: 1
+})
 
-  export interface ProTextProps {
-    text: string
-    copyable?: boolean
-    ellipsis?: boolean
-    lineClamp?: number | string
+export interface ProTextProps {
+  text: string
+  copyable?: boolean
+  ellipsis?: boolean
+  lineClamp?: number | string
+}
+const ellipsis = computed(() => props.ellipsis !== false)
+const copyable = computed(() => props.copyable)
+const maxLines = computed(() => props.lineClamp)
+
+const defaultColor = ref('#3b82f6')
+const copiedColor = ref('#22c55e')
+const bmIconStyle = computed(() => ({
+  display: 'inline-block',
+  verticalAlign: 'middle',
+  cursor: 'pointer'
+}))
+
+const { text: _clipboardText, copy, copied } = useClipboard({ legacy: true })
+
+const isCopied = ref(false)
+
+const handleCopy = () => {
+  if (!props.text.toString()) {
+    return
   }
-  const ellipsis = computed(() => props.ellipsis !== false)
-  const copyable = computed(() => props.copyable)
-  const maxLines = computed(() => props.lineClamp)
+  copy(props.text.toString())
 
-  const defaultColor = ref('#3b82f6')
-  const copiedColor = ref('#22c55e')
-  const bmIconStyle = computed(() => ({
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    cursor: 'pointer'
-  }))
+  isCopied.value = true
+  setTimeout(() => {
+    isCopied.value = false
+  }, 2500)
+
+}
 </script>
 
 <style scoped lang="scss">
-  .bm-text {
-    vertical-align: middle;
-  }
+.bm-text {
+  vertical-align: middle;
+}
 </style>
