dcd4ff770ff3b618d395845e47e661051ddd20ae
lqf <maculosa@163.com>
2024-12-25 15:25:27 +0800

    chore(vite): 优化代码构建尺寸

--- build/procomponent.lib.config.ts
+++ build/procomponent.lib.config.ts
@@ -6,6 +6,13 @@ import baseConfig from './base.config'
 
 export default defineConfig({
   ...baseConfig,
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      },
+    },
+  },
   build: {
     outDir: 'dist',
     lib: {
@@ -15,11 +22,12 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'uno.css', 'naiveUi'],
+      external: ['vue', 'uno.css', 'naive-ui'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
+          'naive-ui': 'naiveUi'
         },
       },
     },
