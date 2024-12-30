d0d98dc1281d80e7537d8fd3fb5ca1d983d50ae5
lqf <maculosa@163.com>
2024-12-30 09:31:44 +0800

    feat(unocss): 添加 shortcuts css 快捷指令

--- uno.config.ts
+++ uno.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   // ... other UnoCSS options
+  shortcuts: {
+    'flex-center': 'flex justify-center items-center',
+    'flex-y': 'flex flex-col',
+    'flex-y-center': 'flex-y justify-center items-center',
+    'border-b': 'border-b border-gray-300',
+    'wh-screen': 'w-screen h-screen',
+  }
 })
