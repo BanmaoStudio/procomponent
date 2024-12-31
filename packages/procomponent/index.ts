c58987faca5652c6b9b5aeaf36d6653cf737273d
lqf <maculosa@163.com>
2024-12-31 11:11:44 +0800

    feat: 将 vue 文件改为 tsx

--- packages/procomponent/index.ts
+++ packages/procomponent/index.ts
@@ -5,13 +5,6 @@
  */
 // import type { App, Plugin } from 'vue'
 
-// import { DrawerFormPlugin } from './components/DrawerForm'
-// import { ModalFormPlugin } from './components/ModalForm'
-// import { ProFormPlugin } from './components/ProForm'
-// import { ProTablePlugin } from './components/ProTable'
-// import { ProTextPlugin } from './components/ProText'
-// import { QueryFilterPlugin } from './components/QueryFilter'
-
 import type { App } from 'vue'
 import * as ProComponents from './components'
 
